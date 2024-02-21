db70d8f883a1eece797778ecf0079ee3bc88d4d6
Helya Moradi <92865004+Helya-Moradi@users.noreply.github.com>
2024-02-21 12:30:50 +0330

    Delete dist directory

--- dist/index.js
+++
@@ -1,9 +0,0 @@
-import Board from "./classes/board.js";
-var board = new Board();
-function keyDownHandler(e) {
-  if (e.code.startsWith('Arrow')) {
-    var direction = e.code.substring(5).toLowerCase();
-    board.moveHandler(direction);
-  }
-}
-window.addEventListener('keydown', keyDownHandler);
--- dist/utils/getBackgrounds.js
+++
@@ -1,5 +0,0 @@
-function getBackgroundColors(value) {
-  var backgroundColors = ['#eee4da', '#eee1c9', '#f3b27a', '#f69664', '#f77c5f', '#f75f3b', '#edd073', '#edcc63', '#edc651', '#eec744', '#ecc230'];
-  return backgroundColors[Math.log2(value) - 1];
-}
-export default getBackgroundColors;
